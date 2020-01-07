9a04db2f2ddd9f04196962a45d22f549f1b8be0d
Marcos Miranda <mmira014@ucr.edu>
2020-01-07 14:05:23 -0800

    Add methods for mileage computation and add ability to use new regulation signal file formats

--- src/core/RegDHandler.java
+++ src/core/RegDHandler.java
@@ -10,15 +10,25 @@ import java.text.ParseException;
  * This class contains the functions used in fetching the regulation-D signals.
  */
 public class RegDHandler {
+  /**
+   * An array of file names in 'workloads/' from which to pull regulation-D signals
+   * These files should contain 1800 entries (1 hour assuming interval of 2 seconds between each signal value)
+   */
+  private static String[] regDFiles = {"regd_highreg","regd_midreg1","regd_midreg2"};
+
     /**
      * An array containing the timestamps present in the regulation-D signal file.
      */
-     private static ArrayList<Double> timeStamps = new ArrayList<Double>();
+  private static ArrayList<Double> timeStamps = new ArrayList<Double>();
 
     /**
      * An array containing the regulation-D values present in the regulation-D signal file.
      */
-     private static ArrayList<Double> regValues = new ArrayList<Double>();
+  private static ArrayList<Double> regValues = new ArrayList<Double>();
+  
+  private static int curHour = 0;
+  private static double mileage = getNewMileage(nextRegDFile());
+  private static int index = 0;
 
     /**
      * Extracts regulation-D signals and corresponding timestamps from a file and populates
@@ -27,42 +37,69 @@ public class RegDHandler {
      * @param fileName - the file from which the data will be extracted. NOTE: The file must be 
      * 			 present in the /workloads/ directory
      */
-    public static void getRegDFromFile(String fileName) {
-	String line = "";
-	String csvSplitChar = ",";
-	BufferedReader bufReader = null;
-		
-	String timeStr = "";
-	Date date = new Date();
-	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  public static void getRegDFromFile(String fileName) {
+        if (fileName.startsWith("regd_")) { 
+		BufferedReader bufReader = null;
+		String line = "";
+		double time = 0;
 
-	try {
-		bufReader = new BufferedReader(new FileReader("workloads/" + fileName));
-		while( (line = bufReader.readLine()) != null) {
-			String[] getLine = line.split(csvSplitChar);
-			if(getLine[1].equals("RegDTest")) { //FIXME: prone to errors if file doesnt start with "RegDTest"
-				continue; // ignore this line
+		double interval = 2.0; // interval of timestamps (2 seconds for data in 'regd_' files)
+		try {
+			bufReader = new BufferedReader(new FileReader("workloads/"+fileName));
+			while( (line = bufReader.readLine()) != null) {
+				line = line.trim();
+				timeStamps.add(time);
+				regValues.add(Double.parseDouble(line));
+				time += interval;
 			}
-			else {
-				//getLine[0] is in sec
-				timeStr = getLine[0];	
-				date = sdf.parse("1970-01-01 " + timeStr);
-				
-				timeStamps.add((double)(date.getTime() - 28800000)/1000); // subtract for timezone diff
-				regValues.add(Double.parseDouble(getLine[1]) );
-			}
+		} catch (Exception e) {
+			System.out.println("[getRegDFromFile]");
+ 			e.printStackTrace();
+			System.exit(0);
 		}
-	
-		//System.out.println("File read complete, printing values:");
-		/*for(int i = 0; i < timeStamps.size(); ++i) {
-			System.out.println(timeStamps.get(i) + ", " + regValues.get(i));
-		}*/
-		//System.out.println("End of values");
-	} catch (IOException e) {
-		e.printStackTrace();
-	} catch (ParseException e) {
-		System.out.println("Problem reading date\n");
+        } else {
+		String line = "";
+		String csvSplitChar = ",";
+		BufferedReader bufReader = null;
+		
+		String timeStr = "";
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		try {
+			bufReader = new BufferedReader(new FileReader("workloads/" + fileName));
+			while( (line = bufReader.readLine()) != null) {
+				String[] getLine = line.split(csvSplitChar);
+				if(getLine[1].equals("RegDTest")) { //FIXME: prone to errors if file doesnt start with "RegDTest"
+					continue; // ignore this line
+				}
+				else {
+					//getLine[0] is in sec
+					timeStr = getLine[0];	
+					date = sdf.parse("1970-01-01 " + timeStr);
+					
+					timeStamps.add((double)(date.getTime() - 28800000)/1000); // subtract for timezone diff
+					regValues.add(Double.parseDouble(getLine[1]) );
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (ParseException e) {
+			System.out.println("Problem reading date\n");
+			System.exit(0);
+		} catch (Exception e) {
+			System.out.println("Problem reading regulation data from file: <" + fileName + ">!");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
+        /*
+	System.out.println("[getRegDFromFile] Contents:\n");
+	System.out.println(timeStamps.toString());
+	System.out.println(regValues.toString());
+	System.exit(0);
+        */
     }
 	
     /**
@@ -91,7 +128,7 @@ public class RegDHandler {
 			System.exit(1);
 		}
 		if(currentTime > time) {
-			// return reg signal corresponding to PREV timestamp
+			// return reg signal corresponding to the previous timestamp
 			return regValues.get(index - 1);
 		}
 		currentTime += diff; //increment currentTime to next timestamp
@@ -101,4 +138,70 @@ public class RegDHandler {
 	System.exit(1);
 	return -1000.0;
     }
+
+  /**
+   * Returns mileage for the given time.
+   * Mileage is defined as the sum of the change in regulation signal over one hour.
+   * Mileage should be updated every hour.
+   * @param curTime The time for which to compute mileage
+   * @return The mileage for the given time.
+   */
+  public static double getCurrentMileage(double curTime) {
+    // check if time is on the hour
+    if(curTime % 3600 == 0) {
+      // verify that time is not the same hour
+      if(curTime / 3600 != curHour) {
+        curHour = (int)curTime / 3600;
+        mileage = getNewMileage(nextRegDFile());
+      }
+    }
+    return mileage;
+  }
+  /**
+   * Computes and returns mileage based on regulation signal data
+   * Note: The regulation data should be over the course of one hour
+   * @param fileName The name of the file containing regulation d signals
+   * @return The new mileage
+   */
+  public static double getNewMileage(String fileName) {
+    BufferedReader bufReader = null;
+    String line = "";
+
+    boolean initialFlag = true;
+    double newMileage = 0.0;
+    double absDifference = 0.0;
+    double newVal = 0.0;
+    double oldVal = 0.0;
+
+    if(!fileName.startsWith("regd_")) {
+      System.out.println("File used to update mileage should start with 'regd_'");
+      System.exit(0);
+    }
+
+    try {
+      bufReader = new BufferedReader(new FileReader("workloads/"+fileName));
+      while( (line = bufReader.readLine()) != null) {
+        line = line.trim();
+        newVal = Double.parseDouble(line);
+        if(initialFlag) { initialFlag = false;}  // don't compute mileage for inital regD value (no previous)
+        else { absDifference = Math.abs(newVal - oldVal);} 
+        newMileage += absDifference;
+        oldVal = newVal;
+      }
+    } 
+    catch (Exception e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+    return newMileage;
+  }
+
+  /** 
+   * Returns a string containing the name of a file containing regulation signals
+   * @return A string containing the name of a file containing regulation signals
+   */
+  public static String nextRegDFile() {
+    if(index > regDFiles.length-1) { index = 0; } // restart at the beginning of files array
+    return regDFiles[index++];
+  }
 }
